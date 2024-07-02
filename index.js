cb31bc5f3ba1f3464746277f94405c5d510e3702
Ankit Biswas <varanasiartist.omg@gmail.com>
2024-07-03 02:11:35 +0530

    file fixed

--- index.js
+++ index.js
@@ -34,7 +34,7 @@ connectToDatabase();
 const server = http.createServer(app);
 const io = new Server(server, {
     cors: {
-        origin: ["http://localhost:3000", "https://collab-writer.vercel.app"],
+        origin: "https://collab-writer.vercel.app",
         methods: ['GET', 'POST']
     }
 });
